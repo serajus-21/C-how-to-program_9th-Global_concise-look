c0915dbc41f866e93ac876e1bcc66d8492eca7d0
serajus-21 <serajussalehin21@gmail.com>
2026-03-01 12:15:49 +0600

    _update

--- _topics/string/print.c
+++
@@ -1,4 +0,0 @@
-#include<stdio.h>
-int main(void) {
-    printf("line1\nline2\n");
-}
--- _topics/string/string-IO.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(void) {
-    char sst[3];
-    // scanf("%s", sst);
-    fgets(sst, sizeof(sst), stdin);
-    printf("%s\n", sst);
-}
